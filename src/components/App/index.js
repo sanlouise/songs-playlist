75db2c7876f11e289d50ccbeb0268ebc3eb7c91e
kelsonic <kgadams93@gmail.com>
2017-08-23 14:30:53 -0400

    Add pulse animation and update shelling color

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -49,7 +49,7 @@ class App extends Component {
   }
 
   displaySongs = (songs) => {
-    // this.setState({ songs, shelling: false });
+    this.setState({ songs, shelling: false });
   }
 
   render() {
